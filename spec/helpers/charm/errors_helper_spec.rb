3bd3dddf22fc8c4214d4068d126ea39988a3c871
Vladimir Sazhin <author@cannie.net>
2013-07-15 02:12:57 +0400

    update

---
+++ spec/helpers/charm/errors_helper_spec.rb
@@ -0,0 +1,11 @@
+require 'helper'
+
+describe Charm::ErrorsHelper do
+  describe '#errors_for' do
+    pending
+  end
+
+  describe '#render_errors' do
+    pending
+  end
+end
---
+++ spec/helpers/charm/flash_messages_helper_spec.rb
@@ -0,0 +1,11 @@
+require 'helper'
+
+describe Charm::FlashMessagesHelper do
+  describe '#render_flash_message' do
+    pending
+  end
+
+  describe '#render_flash_messages' do
+    pending
+  end
+end
